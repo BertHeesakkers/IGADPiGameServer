1a1ecf554d8ee1d8588adb5d202f03d6a004665c
Bert Heesakkers <heesakkers.b@nhtv.nl>
2018-03-23 16:53:07 +0100

    [SERVERAPP]
    - reinstated use of log file parameter for server application

--- Source/ServerApp/main.cpp
+++ Source/ServerApp/main.cpp
@@ -9,18 +9,19 @@
 #include <string>
 #include <sstream>
 
-#include <boost/format.hpp>
-
 namespace
 {
 	const std::string g_ParameterFile = "parameters";
 	const std::string g_ParameterFileShort = "p";
+	const std::string g_LogFile = "logfile";
+	const std::string g_LogFileShort = "l";
 }
 
 int main(int a_ArgC, char *a_ArgV[])
 {
 	CommandLineParser commandLineParser;
 	commandLineParser.AddParameter(ECommandLineItemType_Optional, g_ParameterFile, g_ParameterFileShort);
+	commandLineParser.AddParameter(ECommandLineItemType_Optional, g_LogFile, g_LogFileShort);
 	const CommandLine &commandLine = commandLineParser.Parse(a_ArgC, a_ArgV);
 
 	std::string parameterfile = "data/server_parameters.json";
@@ -29,12 +30,27 @@ int main(int a_ArgC, char *a_ArgV[])
 		parameterfile = commandLine.GetParameter(HashedString(g_ParameterFile));
 	}
 
-	ConsoleLogger logger;
-	ServerApp app(logger);
+	std::string logfile = "";
+	bool useLogFile = false;
+	if (commandLine.HasParameter(HashedString(g_LogFile)))
+	{
+		logfile = commandLine.GetParameter(HashedString(g_LogFile));
+		useLogFile = true;
+	}
+
+	ILogger *logger = nullptr;
+	if (useLogFile)
+	{
+		logger = new FileLogger(logfile);
+	}
+	else
+	{
+		logger = new ConsoleLogger();
+	}
+
+	ServerApp app(*logger);
 	app.Initialize(parameterfile);
 	app.StartServer(Sync);
+	delete logger;
 	return 0;
 }
-
-//http://www.melvilletheatre.com/articles/cstuff/3.html
-//http://techlister.com/linux/creating-menu-with-ncurses-in-c/1293/
