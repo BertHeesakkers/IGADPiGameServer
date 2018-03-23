0bc560625ee0f1b3dabbbea8e2638f3eadcd74ac
Bert Heesakkers <bertheesakkers@gmail.com>
2018-03-23 15:51:03 +0000

    - adding Linux install scripts

---
+++ Scripts/StartIGADPiGameServerService.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Starting IGADPiGameServer service..."
+sudo systemctl start IGADPiGameServer.service
+
---
+++ Scripts/StopIGADPiGameServerService.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Stopping IGADPiGameServer service..."
+sudo systemctl stop IGADPiGameServer.service
+
